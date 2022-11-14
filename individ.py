461ad0af00b5f66795faabadd894d20254d6b325
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-11-14 15:58:06 +0300

    Add files via upload

---
+++ individ.py
@@ -0,0 +1,23 @@
+from json.tool import main
+
+if __name__ == '__main__':
+
+
+    def decorator(func, start=5):
+
+        def inner(*args, **kwargs):
+            f = func(*args, **kwargs)
+            print('Cумма введенных чисел: {}.'.format(start + f))
+
+        return inner
+
+
+    @decorator
+    def out(s):
+        str_numb = list(map(int, s.split()))
+        summa = sum(str_numb)
+        return summa
+
+    str_text = \
+        input('Введите числа через пробел: ')
+    out(str_text)
---
+++ primer2.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    def benchmark(func):
+        import time
+
+        def wrapper():
+            start = time.time()
+            func()
+            end = time.time()
+            print('[*] Время выполнения: {} секунд.'.format(end-start))
+        return wrapper
+
+    @benchmark
+    def fetch_webpage():
+        import requests
+        webpage = requests.get('https://google.com')
+
+    fetch_webpage()
