930d16ad2d38f30f0f4d56f9c61bb5a4695464bc
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-11-14 15:39:09 +0300

    Add files via upload

---
+++ individ.py
@@ -0,0 +1,15 @@
+def decorator(func, start = 5):
+
+    def inner(*args, **kwargs):
+        f = func(*args, **kwargs)
+        print('Cумма введенных чисел: {}.'.format(start + f))
+    return inner
+
+@decorator
+def out(s):
+    str_numb = list(map(int, s.split()))
+    summa = sum(str_numb)
+    return summa
+
+str_text = input('Введите числа через пробел: ')
+out(str_text)
