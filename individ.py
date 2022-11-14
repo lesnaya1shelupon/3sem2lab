5527fe1c344f2bb65a2f7bccc437b291f6521c53
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-11-14 16:50:28 +0300

    Update individ.py

--- individ.py
+++ individ.py
@@ -1,24 +1,16 @@
-s (15 sloc)  529 Bytes
-
 from json.tool import main
 
 if __name__ == '__main__':
-
-
-
     def decorator(func, start = 5):
 
 
         def inner(*args, **kwargs):
             f = func(*args, **kwargs)
             print('Cумма введенных чисел: {}.'.format(start + f))
-
         return inner
 
 
     @decorator
-
-
     def out(s):
         str_numb = list(map(int, s.split()))
         summa = sum(str_numb)
@@ -27,4 +19,3 @@ if __name__ == '__main__':
     str_text = \
         input('Введите числа через пробел: ')
     out(str_text)
-    
